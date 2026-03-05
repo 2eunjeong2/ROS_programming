8ad39038f809115075b6eb2ad378a41a4e02374c
2eunjeong2 <qwert45640@gmail.com>
2026-03-05 00:41:58 -0800

    feat: rosbag play + Subscriber 연동 확인

--- beginner_tutorials/scripts/pose_listener.py
+++ beginner_tutorials/scripts/pose_listener.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python3
-
 import rospy
+from turtlesim.msg import Pose
 
-from turtlesim.msg import pose
 def callback(msg):
-
-    rospy.loginfo("위치: x=%.2f, y=%.2f, 방향=%.2f", msg.______, msg.______, msg.______)
+    rospy.loginfo("위치: x=%.2f, y=%.2f, 방향=%.2f", msg.x, msg.y, msg.theta)
 
 def listener():
-
     rospy.init_node('pose_listener')
-
-    rospy.Subscriber('/turtle1/pose', pose, callback)
-
+    rospy.Subscriber('/turtle1/pose', Pose, callback)
     rospy.spin()
 
 if __name__ == '__main__':
-
     listener()
